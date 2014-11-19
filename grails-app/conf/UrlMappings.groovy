705fcdc0acbecf58c3c03d9955f72a664d90b20e
Jeff Scott Brown <jbrown@pivotal.io>
2014-11-19 10:11:18 -0600

    add mapping for /signin

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -8,6 +8,11 @@ class UrlMappings {
         }
 
         "/"(controller: "web")
+
+        // it isn't clear what your real requirments are
+        // so this may or may not be what you want
+        "/signin"(controller: 'web', action: 'signin')
+
         "500"(view:'/error')
 	}
 }
