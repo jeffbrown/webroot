d066c3246fcd1a367ca8575fd973580039e1512a
Jeff Scott Brown <jbrown@pivotal.io>
2014-11-19 10:27:58 -0600

    answering another question

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,17 +7,19 @@ class UrlMappings {
             }
         }
 
-        "/"(controller: "web")
+        //"/"(controller: "web")
 
         // it isn't clear what your real requirments are
         // so this may or may not be what you want
-        "/signin"(controller: 'web', action: 'signin')
+        //"/signin"(controller: 'web', action: 'signin')
 
         // You might also be looking for something like this instead
         // which may or may not be problematic depending on other
         // factors in your app...
         // "/$action"(controller: 'web')
 
+        // is this what you want?
+        "/$action?"(controller: "web")
 
         "500"(view:'/error')
 	}
